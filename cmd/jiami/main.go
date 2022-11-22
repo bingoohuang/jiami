ffb513dfe9f3e9d986fb7e58c8ba34dbe2b38be8
bingoohuang <bingoo.huang@gmail.com>
2022-11-22 15:36:27 +0800

    cmd

---
+++ cmd/jiami/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/base64"
+	"log"
+
+	"github.com/bingoohuang/jiami"
+	"github.com/spf13/pflag"
+	"github.com/vmihailenco/msgpack/v5"
+)
+
+func main() {
+	input := pflag.StringP("input", "i", "", "input string")
+	passphrase := pflag.StringP("passphrase", "p", "", "passphrase")
+	pflag.Parse()
+
+	d, err := base64.StdEncoding.DecodeString(*input)
+	if err != nil {
+		log.Fatalf("base64.StdEncoding.DecodeString failed: %v", err)
+	}
+
+	encoded := &jiami.Encoded{}
+	if err := msgpack.Unmarshal(d, encoded); err != nil {
+		log.Fatalf("msgpack.Unmarshal failed: %v", err)
+	}
+
+	key := &jiami.Key{Passphrase: []byte(*passphrase), Salt: encoded.Salt}
+	if err := key.Init(); err != nil {
+		log.Fatalf("key.Init failed: %v", err)
+	}
+
+	plain, err := jiami.NewAesGcm().Decrypt(key, encoded)
+	if err != nil {
+		log.Fatalf("Decrypt failed: %v", err)
+	}
+
+	log.Printf("Plain: %s", plain)
+}
--- go.mod
+++ go.mod
@@ -3,12 +3,15 @@ module github.com/bingoohuang/jiami
 go 1.19
 
 require (
+	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.8.1
+	github.com/vmihailenco/msgpack/v5 v5.3.5
 	golang.org/x/crypto v0.3.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
