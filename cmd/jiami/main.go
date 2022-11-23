d9d1f5f029b454d46e80b11fe3a1f6c38fc00649
bingoohuang <bingoo.huang@gmail.com>
2022-11-23 08:28:30 +0800

    decode base64 compatible

--- cmd/jiami/main.go
+++ cmd/jiami/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/base64"
+	"io"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/bingoohuang/jiami"
 	"github.com/spf13/pflag"
@@ -15,7 +18,7 @@ func main() {
 	passphrase := pflag.StringP("passphrase", "p", "", "passphrase")
 	pflag.Parse()
 
-	d, err := base64.StdEncoding.DecodeString(*input)
+	d, err := DecodeString(*input)
 	if err != nil {
 		log.Fatalf("base64.StdEncoding.DecodeString failed: %v", err)
 	}
@@ -40,4 +43,41 @@ func main() {
 	}
 
 	log.Printf("Plain: %s", plain)
+}
+
+// DecodeString decode string which is in base64 format ( any one of StdEncoding/URLEncoding/RawStdEncoding/RawURLEncoding).
+func DecodeString(src string) ([]byte, error) {
+	var b bytes.Buffer
+	if _, err := Decode(&b, strings.NewReader(src)); err != nil {
+		return nil, err
+	}
+	return b.Bytes(), nil
+}
+
+// Decode copies io.Reader which is in base64 format ( any one of StdEncoding/URLEncoding/RawStdEncoding/RawURLEncoding).
+func Decode(dst io.Writer, src io.Reader) (int64, error) {
+	return io.Copy(dst, base64.NewDecoder(base64.RawStdEncoding, &rawStdEncodingReader{Reader: src}))
+}
+
+type rawStdEncodingReader struct{ io.Reader }
+
+// StdEncoding：RFC 4648 定义的标准 BASE64 编码字符集，结果填充=，使字节数为4的倍数
+// URLEncoding：RFC 4648 定义的另一 BASE64 编码字符集，用 - 和 _ 替换了 + 和 /，用于URL和文件名，结果填充=
+// RawStdEncoding：同 StdEncoding，但结果不填充=
+// RawURLEncoding：同 URLEncoding，但结果不填充=
+func (f *rawStdEncodingReader) Read(p []byte) (int, error) {
+	n, err := f.Reader.Read(p)
+	for i := 0; i < n; i++ {
+		switch p[i] {
+		case '-':
+			p[i] = '+'
+		case '_':
+			p[i] = '/'
+		case '=':
+			n = i
+			return n, err
+		}
+	}
+
+	return n, err
 }
